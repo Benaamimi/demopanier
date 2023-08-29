9916bba136a182a309d2e8b069b178f9b242e580
Tarik_B <tarik_b@DESKTOP-7S7Q8AE.doranco.edu>
2023-08-29 10:58:47 +0200

    😃 Add service

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Repository\ProductRepository;
+use App\Service\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,95 +13,28 @@ class PanierController extends AbstractController
 {
 
     #[Route('/cart', name: 'cart')]
-    public function index(RequestStack $rs, ProductRepository $repo): Response
+    public function index(CartService $cs): Response
     {
-        $session = $rs->getSession();
-        $cart = $session->get('cart', []);
-
-        //Je vais créer un nouveau tableau qui contiendra des objets Product et les quantité de chaque objet
-        $cartWithData = [];
-
-        //Pour chaque id qui se trouve dans le tableau $cart, on ajoute une case(tableau) dans cartWithData, qui est un tableau multidimensionnel
-        foreach ($cart as $id => $quantity) {
-            $cartWithData[] = [
-                'product' => $repo->find($id),
-                'quantity' => $quantity
-            ];
-        }
-
-        $total = 0; // j'initialise mon total
-
-        foreach ($cartWithData as $item) {
-            $sousTotal = $item['product']->getPrice() * $item['quantity'];
-
-            // $total =  $total + $sousTotal;
-            //or
-            $total += $sousTotal;
-
-        }
-
-
         return $this->render('panier/index.html.twig', [
-            'items' => $cartWithData,
-            'total' => $total
+            'items' => $cs->getCartWithData(),
+            'total' => $cs->getTotal()
         ]);
     }
 
 
     #[Route('/cart/add/{id}', name: 'cart_add')]
-    public function add($id, RequestStack $rs)
+    public function add($id, CartService $cs)
     {
-        //Nous allons récupèrer une session grâce a la class ResquestStack
-        $session = $rs->getSession();
-
-        //Je recupère la session actuel 'cart' s'il existe ou un tableau vide
-        $cart = $session->get('cart', []);
-        $qt = $session->get('qt', 0);
-
-        //si le produit existe déjà, j'incrémente sa quantité, sinon j'initialise a 1
-        if (!empty($cart[$id])) {
-            $cart[$id]++;
-            $qt++;
-        } else {
-            $qt++;
-            $cart[$id] = 1;
-        }
-        // dans mon tableau $cart, à la case $id je donne la valeur 1
-        // indice => valeur // idproduit => QuantitéDuproduitDansLePanier
-
-        $session->set('cart', $cart);
-        $session->set('qt', $qt);
-
-        //je sauvegarde l'etat de mon panier en session a l'attribut de session 'cart'
-
+        $cs->add($id);
         return $this->redirectToRoute('home');
     }
 
+
     #[Route('/cart/retirer/{id}', name:'cart_remove')]
-    public function remove($id, RequestStack $rs)
+    public function remove($id, CartService $cs)
     {
-        $session = $rs->getSession();
-        $cart = $session->get('cart', []);
-        $qt = $session->get('qt', 0);
-
-
-        // si l'id existe dans le panier, je le supprime avec unset
-        if (!empty($cart[$id])) 
-        {
-            $qt-=$cart[$id];
-            unset($cart[$id]);
-        }
-
-        if($qt < 0)
-        {
-            $$qt = 0;
-        }
-
-        $session->set('qt', $qt);
-        $session->set('cart', $cart);
-
+        $cs->remove($id);
         return $this->redirectToRoute('cart');
-
-        
+  
     }
 }
